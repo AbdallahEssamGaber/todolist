ded2927778742991cef1c5380a72910d041f259f
AbdallahEssamGaber <abdallahgaber441@gmail.com>
2022-02-04 12:03:30 +0200

    Styling half of the page and making archive page

--- app.js
+++ app.js
@@ -6,34 +6,131 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+const mongoose = require("mongoose");
 
-const newTasks = ["Plan", "Study", "Eat Food"];
-const workTasks = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+// const newTasks = ["Plan", "Study", "Eat Food"];
+// const workT asks= [];
+
+const itemsSchema = {
+  name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+const ItemArchive = mongoose.model("itemsArchive", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todolist!"
+});
+
+const item2 = new Item({
+  name: "Hit + in order to add a new item."
+});
+
+const item3 = new Item({
+  name: "welcome again!"
+});
+
+const defaultItems = [item1, item2, item3];
+
+var deleteItemName = "";
+
 
 
 
 app.get("/",function(req, res){
 
-
-  res.render("list", {listTitle: date.getDate(), newTasks: newTasks});
+  Item.find({}, function(err, found){
+    if(found.length === 0){
+      Item.insertMany(defaultItems,function(err){
+        if(err) console.log(err);
+        else console.log("Success");
+      });
+      res.redirect("/");
+    }else{
+      res.render("list", {listTitle: date.getDate(), newTasks: found, route: "/"});
+    }
+  })
 
 
 });
 
+
+app.get("/archive", function(req, res){
+  ItemArchive.find({}, function(err, found){
+    if(err) console.log(err);
+    else res.render("list", {listTitle: "Archive", newTasks: found, route: "/archive"});
+  });
+
+});
+
+
 app.post("/", function(req, res){
 
-  newTask = req.body.newTask;
+  const newTask = req.body.newTask;
+
+  const item = new Item({
+    name: newTask
+  })
+
+  item.save();
+  res.redirect("/");
+
+});
 
 
-  if(req.body.list === "Work"){
-    workTasks.push(newTask);
-    res.redirect("/work");
-    return;
-  }
+app.post("/delete", function(req, res){
+  const idChecked = req.body.deleteCheckbox;
+  Item.findById(idChecked, function(err, deltedItem){
+    deleteItemName = deltedItem.name;
+    const itemDelted = new ItemArchive({
+      name: deleteItemName
+    });
+    itemDelted.save();
+
+  });
 
 
-  newTasks.push(newTask);
-  res.redirect("/")
+  Item.findByIdAndRemove(idChecked, function(err){
+    if(err){
+      console.log(err);
+    }
+
+    else{
+          console.log("item " + idChecked + "got deleted");
+          res.redirect("/");
+    }
+
+  });
+
+});
+
+
+app.post("/restore", function(req, res){
+  const idUnChecked = req.body.restoreCheckbox;
+  console.log(idUnChecked);
+  // ItemArchive.findById(idUnChecked, function(err, restoreItem){
+  //   restoreItemName = restoreItem.name;
+  //   const itemRestored = new Item({
+  //     name: restoreItemName
+  //   });
+  //   itemRestored.save();
+  // });
+  //
+  //
+  // ItemArchive.findByIdAndRemove(idUnChecked, function(err){
+  //   if(err){
+  //     console.log(err);
+  //   }
+  //
+  //   else{
+  //         console.log("item " + idUnChecked + "got deleted");
+  //         res.redirect("/archive");
+  //   }
+  //
+  // });
+  res.redirect("/archive")
 });
 
 
